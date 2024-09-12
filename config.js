2f78f42d5d8d6aa63d69c33f4b0c63ecb8813dbd
Shay Ronen <shayronen@gmail.com>
2024-09-12 13:18:19 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+module.exports = {
+    tradingIsActive: function() {
+        // Logic to determine if trading is active (simplified for this example)
+        return true; 
+    }
+};
